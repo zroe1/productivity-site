e41676908c411a0649389c38c0f1cd21b7ce0d5c
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-10 17:31:21 -0500

    added homepage with header

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,42 @@
+import styles from './Header.module.css'
+import Image from 'next/image'
+
+{/* <header>
+  <img height="35px" class="icon" src="graphlogo.png" alt="">
+  <h1 class="header-title">zephaniahsdata.com</h1>
+  <menu class="desktop-menu">
+    <a class="header-menu desktop-menu-link" href="#">Data</a>
+    <a class="header-menu desktop-menu-link" href="#">Results</a>
+    <a class="header-menu desktop-menu-link" href="#">Progress</a>
+    <a class="header-menu desktop-menu-link" href="#">About</a>
+    <a class="header-menu desktop-menu-link" href="#">GitHub</a>
+    <img src="github-mark/github-mark.png" alt="github" class="github-logo">
+  </menu>
+</header>
+<menu class="mobile-menu">
+  <a class="header-menu" href="#">Data</a>
+  <a class="header-menu" href="#">Results</a>
+  <a class="header-menu" href="#">Progress</a>
+  <a class="header-menu" href="#">About</a>
+</menu> */}
+
+const Header = () => {
+  return (
+    <header className={styles.pageHead}>
+      {/*eslint-disable-next-line @next/next/no-img-element*/}
+      <img className={styles.icon} src="graphlogo.png" alt="logo"></img>
+      <div className={styles.headerTitle}>zephaniahsdata.com</div>
+      <nav className={styles.desktopMenu}>
+        <a className={styles.navLink}>Data</a>
+        <a className={styles.navLink}>Results</a>
+        <a className={styles.navLink}>Progress</a>
+        <a className={styles.navLink}>About</a>
+        <a className={styles.navLink}>GitHub</a>
+        {/*eslint-disable-next-line @next/next/no-img-element*/}
+        <img src="github-mark.png" alt="github" className={styles.githubLogo}></img>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import CountButton from './components/CountButton/CountButton'
+import SearchBar from './components/SearchBar/SearchBar'
+import styles from './page.module.css'
+
+const products = [
+    'tooth paste',
+    'tooth brush',
+    'mouth wash',
+    'dental floss',
+    'mouth guard',
+]
+  
+
+export default function Home() {
+
+  const myItem = 'mike'
+  
+  return (
+    // <CountButton incrementBy={5} buttonColor={'blue'} />
+    <></>
+  )
+}
